ed06491e463197a9b1f987fd5487dc195b224c32
ramazancoskun <112482946+ramazancoskun@users.noreply.github.com>
2022-08-30 15:49:39 +0300

    Update ödev2.sql

--- ödev2.sql
+++ ödev2.sql
@@ -3,4 +3,4 @@ SELECT * FROM film WHERE replacement_cost BETWEEN 12.99 AND 16.99;
 --.actor tablosunda bulunan first_name ve last_name sütunlardaki verileri first_name 'Penelope' veya 'Nick' veya 'Ed' değerleri olması koşuluyla sıralayınız. ( IN operatörünü kullanınız.)
 SELECT first_name, last_name from actor WHERE first_name IN('Penelope', 'Ed', 'Nick');
 film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99, 2.99, 4.99 VE replacement_cost 12.99, 15.99, 28.99 olma koşullarıyla sıralayınız. ( IN operatörünü kullanınız.)
-SELECT * from film WHERE first_name IN(0.99, 2.99, 4.99) AND replacement_cost IN(12.99, 15.99, 28.99);
+SELECT * from film WHERE rental_rate IN(0.99, 2.99, 4.99) AND replacement_cost IN(12.99, 15.99, 28.99);
